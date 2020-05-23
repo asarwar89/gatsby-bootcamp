9189e9c1f69d2a8a09d217c2cd1f0cc59997638a
Sarwar Ahmed <asarwar89@yahoo.com>
2020-05-23 23:36:26 +1000

    Updated home page text

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home"/>
             <h1>Hello,</h1>
             <h2>I'm Sarwar, a full-stack developer living in beautiful Westmead.</h2>
-            <p>Need a developer? <Link to="contact">contact me</Link></p>
         </Layout>
     )
 }
